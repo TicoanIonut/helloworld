2c2d4edad6b9de6fca397216547b1b6b811c6e23
Ticoan Ionut <96721825+icefusionlol@users.noreply.github.com>
2022-01-18 22:28:18 +0200

    18.01.22

---
+++ ex8.1.py
@@ -0,0 +1,16 @@
+def chop(t):
+    del t[0]
+    del t[-1]
+let=[1, 2, 3, 1, 2, 3, 1, 2, 3]
+chop(let)
+print('chop',let)
+
+
+
+def mid(s):
+    del s[0]
+    del s[-1]
+lll=[1, 2, 3, 1, 2, 3, 1, 2, 3]
+mid(lll)
+print('mid',lll)
+

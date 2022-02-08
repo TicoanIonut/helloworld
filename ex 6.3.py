7cf9cabd6230583be9e8e085961907176ad0944b
Ticoan Ionut <96721825+icefusionlol@users.noreply.github.com>
2022-02-08 20:16:01 +0200

    08.02.22

--- ex 6.3.py
+++ ex 6.3.py
@@ -1,9 +1,9 @@
-def count(count):
+def count():
     word = 'banana'
     count = 0
     for letter in word:
         if letter == 'a':
             count = count + 1
     print(count)
-count(count)
+count()
 
